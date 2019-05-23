8e7c38b3eea6499d9210228f840a53a81237d0ac
chenlizan <clz_318@live.com>
2019-05-23 20:02:25 +0800

    5.23

--- bin/ijz-react-tools.js
+++ bin/ijz-react-tools.js
@@ -1,25 +1,31 @@
 #!/usr/bin/env node
 
+const path = require('path');
 const program = require('commander');
-const dll_config = require('../script/webpack.dll.config');
 const spawn = require('cross-spawn');
+const version = require('../package').version;
 
-// 定义版本和参数选项
 program
-    .version('0.1.0', '-v, --version')
-    .option('dll', 'build dll');
+    .version(version, '-v, --version')
+    .option('dll', 'build dll')
+    .option('dev', 'dev')
+    .option('prod', 'prod');
 
-// 必须在.parse()之前，因为node的emit()是即时的
-program.on('--help', function(){
-    console.log('  Examples:');
-    console.log('');
-    console.log('    this is an example');
-    console.log('');
-});
 
 program.parse(process.argv);
 
-if(program.dll) {
-    console.log('building dll');
-    spawn('webpack', ['--config', 'webpack.dll.config'], { stdio: 'inherit' });
+if (program.dll) {
+    console.info('start building dll');
+    const script = 'webpack.dll.config';
+    spawn('webpack', ['--config', path.join(__dirname + '/../scripts/' + script)], {stdio: 'inherit'});
+}
+
+if(program.dev){
+
+}
+
+if(program.prod) {
+    console.info('start building dll');
+    const script = 'webpack.prod.config';
+    spawn('webpack', ['--config', path.join(__dirname + '/../scripts/' + script)], {stdio: 'inherit'});
 }
