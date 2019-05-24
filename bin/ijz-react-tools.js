618cfeb2df836106a21d1a77b10d9066fcaf96c4
chenlizan <clz_318@live.com>
2019-05-24 17:42:56 +0800

    5.24

--- bin/ijz-react-tools.js
+++ bin/ijz-react-tools.js
@@ -8,8 +8,8 @@ const version = require('../package').version;
 program
     .version(version, '-v, --version')
     .option('dll', 'Builds the dll for development')
-    .option('dev', 'Runs the app in development mode')
-    .option('prod', 'Builds the app for production');
+    .option('start', 'Runs the app in development mode')
+    .option('build', 'Builds the app for production');
 
 program.parse(process.argv);
 
@@ -19,13 +19,13 @@ if (program.dll) {
     spawn('cross-env', ['NODE_ENV=development', 'webpack', '--config', path.join(__dirname + '/../scripts/' + script)], {stdio: 'inherit'});
 }
 
-if (program.dev) {
+if (program.start) {
     const script = 'webpack.dev.config';
     console.info('Runs the app in development mode');
     spawn('cross-env', ['NODE_ENV=development', 'webpack-dev-server', '--config', path.join(__dirname + '/../scripts/' + script)], {stdio: 'inherit'});
 }
 
-if (program.prod) {
+if (program.build) {
     const script = 'webpack.prod.config';
     console.info('Builds the app for production');
     spawn('cross-env', ['NODE_ENV=production', 'webpack', '--config', path.join(__dirname + '/../scripts/' + script)], {stdio: 'inherit'});
